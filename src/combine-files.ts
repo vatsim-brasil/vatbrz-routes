eb8d02ade2623f84d079d2e168064fad1b749bbd
Joao Duarte <joao.pedro.hsd@gmail.com>
2025-10-17 21:12:23 -0300

    ci: Add manual flow and fix script

--- src/combine-files.ts
+++ src/combine-files.ts
@@ -2,8 +2,22 @@ import { readFile } from "node:fs/promises";
 import { simBriefFlightSchema } from "./types.ts";
 
 const date = process.argv[2];
-if(!date) throw new Error('Date is required as first argument');
+if (!date) throw new Error("Date is required as first argument");
 
 const rplFile = await readFile("data/rpl.json", "utf-8");
 const rplFileContents = simBriefFlightSchema.array().parse(JSON.parse(rplFile));
-console.log(JSON.stringify({ valid: date.toUpperCase(), data: [...rplFileContents] }, null, 2));
+
+const manualFile = await readFile("data/manual.json", "utf-8");
+const manualFileContents = simBriefFlightSchema
+	.array()
+	.parse(JSON.parse(manualFile));
+console.log(
+	JSON.stringify(
+		{
+			valid: date.toUpperCase(),
+			data: [...rplFileContents, ...manualFileContents],
+		},
+		null,
+		2,
+	),
+);
