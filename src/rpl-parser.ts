feaede0a14369bb25481ca23ff2f16bc0dee26a9
Joao Duarte <joao.pedro.hsd@gmail.com>
2025-10-25 23:04:06 -0300

    refactor: redorder imports

--- src/rpl-parser.ts
+++ src/rpl-parser.ts
@@ -1,7 +1,7 @@
 import path from "node:path";
-import { rplToSimbriefFlight } from "./lib/rpl-to-simbrief-flight.ts";
-import { groupSimbriefFlights } from "./lib/group-simbrief-flights.ts";
 import { fetchRplFlightData } from "./lib/fetch-rpl-flight-data.ts";
+import { groupSimbriefFlights } from "./lib/group-simbrief-flights.ts";
+import { rplToSimbriefFlight } from "./lib/rpl-to-simbrief-flight.ts";
 
 const rplInputFile = path.resolve("./data/rpl-source.json");
 const flights = await fetchRplFlightData(rplInputFile);
